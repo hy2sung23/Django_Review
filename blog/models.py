6e8f9334200a440c724141099b97aea3d281e4e4
hyeseong <hystom38@gmail.com>
2020-06-21 23:39:35 +0900

    second

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,4 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_publish = models.BooleanField(default=False)
